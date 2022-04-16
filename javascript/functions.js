28b73a0f99fa7940e85e8717c5e41f8db4f7a85e
chrisWO92 <chrisworkingonline@gmail.com>
2022-04-16 14:05:54 -0300

    Commit parcial

--- javascript/functions.js
+++ javascript/functions.js
@@ -42,23 +42,178 @@ console.log('diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]): ', diffArray([1, 2, 3, 5]
 const destroyer = (arr, ...args) => {
     let argsArr = [...args];
     let newArr = [...arr];
-    console.log(argsArr);
-    let result = [];
     return newArr.filter(elem => {
-        for (let arg of argsArr){
-            if (!(elem == arg)){
-                return elem;
-            }
-        };
-        /* argsArr.forEach(arg => {
-            if (arg != elem){
-               result.push(elem);
-               
-            }
-        }) */
+        if (argsArr.indexOf(elem) == -1){
+            return elem;
+        }
     });
     /* return result; */
 }
 
 console.log('destroyer([1, 2, 3, 1, 2, 3], 2, 3): ', destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+
+
+
+////////////////////////////
+
+function whatIsInAName(collection, source) {
+    let keys = Object.keys(source);
+    return collection.filter(elem => {
+        let pivot = false;
+        let pivotG = keys.every(key => {
+            if ((elem[key]) && (elem[key] == source[key])) {
+                pivot = true;
+                return pivot;
+            };
+        });
+        console.log(pivotG);
+        return pivotG;
+    });
+}
+  
+console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
+
+
+console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 }));
+
+
+
+///////////////////////////////
+
+let testStr = "";
+
+
+const verificadorReg = (char, ...regs) => {
+    let regsArr = [...regs];
+    
+    let result = regsArr.some(reg => {
+        return reg.test(char);
+    });
+    return result;
+}
+
+function spinalCase(str) {
+    let reg1 = /_/;
+    let reg2 = /\s/;
+    let reg3 = /-/;
+    let reg4 = /\w/;
+    let arr = [];
+
+    const verificadorReg = (char, ...regs) => {
+        let regsArr = [...regs];
+        
+        let result = regsArr.some(reg => {
+            return reg.test(char);
+        });
+        return result;
+    }
+
+    let newStr = str.slice().trim().split("");
+    newStr.forEach(elem => {
+        if (newStr.indexOf(elem) == 0){
+            arr.push(elem.toLowerCase());
+        }else {
+            if (verificadorReg(elem, reg1, reg2, reg3)){
+                arr.push("-");
+            }else {
+                arr.push(elem);
+            } 
+        }   
+    });
+    let result = [];
+    let index = 0;
+    arr.forEach(elem => {
+        if (elem != elem.toLowerCase()){
+            if (arr[index-1] != "-"){
+                result.push('-', elem.toLowerCase());
+                
+            }else {
+                result.push(elem.toLowerCase());
+                
+            }
+        }else {
+            result.push(elem);
+            
+        }
+        index++;        
+    });
+    return result.join("");
+}
+
+console.log(spinalCase('This Is Spinal Tap'));
+console.log(spinalCase('thisIsSpinalTap'));
+
+
+
+/////////////////////////////////
+
+function translatePigLatin(str) {
+
+    let reg1 = /(^[aeiou]+)(\w+)/;
+    let reg2 = /(^[^aeiou]+)(\w+)/;
+    let reg3 = /([aeiou]+)/;
+    let regResult = [];
+
+    if (reg3.test(str)){
+        if (reg1.test(str)){
+            regResult = str.match(reg1);
+            return regResult[0] + "way";
+        }else {
+            regResult = str.match(reg2);
+            return regResult[2] + regResult[1] + "ay";
+        }
+    }else{
+        return str + "ay";
+    }
+}
+
+console.log(translatePigLatin("consonant"));
+console.log(translatePigLatin("paragraphs"));
+console.log(translatePigLatin("glove"));
+console.log(translatePigLatin("algorithm"));
+console.log(translatePigLatin("eight"));
+console.log(translatePigLatin("rhythm"));
+console.log(translatePigLatin("schwartz"));
+
+
+/* let reg1 = /^[aeiou]{1}/;
+let textStr = 'adollknasdays';
+console.log(reg1.test(textStr));
+console.log(textStr.match(reg1));
+
+let reg2 = /^[^aeiou]{1,8}/;
+let textStr2 = 'dpollknasdays';
+console.log(reg2.test(textStr2));
+console.log(textStr2.match(reg2));
+
+
+let reg3 = /(?<=([aeiou]))\w+/;
+let textStr3 = 'ooollknasdays';
+console.log(reg3.test(textStr3));
+console.log(textStr3.match(reg3));
+
+let reg4 = /(^[aeiou]+)(\w+)/;
+let textStr4 = 'ooollknasdays';
+console.log(reg3.test(textStr4));
+console.log(textStr3.match(reg4));
+
+console.log(textStr2.match(reg4)); */
+
+
+///////////////////////////
+
+function myReplace(str, before, after) {
+    let afterStr = "";
+    if (before != before.toLowerCase()){
+        afterStr = after.slice(0,1).toUpperCase() + after.slice(1,);
+    }else {
+        afterStr = after.toLowerCase();
+    }
+    return str.replace(before, afterStr);
+}
+  
+console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"));
+
+
+////////////////////////////////
 
